040e546d199d959f0a7974c0c785bb4ec496e0fe
Anshika31sharma <sharma.anshika7251@gmail.com>
2024-03-07 16:27:26 +0530

    navbar added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AiFillHome , AiOutlineShopping, AiOutlineTags } from 'react-icons/ai';
+
+const Navbar = ({ setActivePage }) => {
+  const handleTabClick = (page) => {
+    setActivePage(page);
+  };
+
+  return (
+    <nav className="bg-white p-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 text-xl">
+          <button
+            className="flex items-center  hover:underline underline-offset-1  hover:font-bold "
+            onClick={() => handleTabClick('dashboard')}
+          >
+            <AiFillHome className="mr-2 " /> Dashboards
+          </button>
+          <button
+            className="flex items-center  hover:underline underline-offset-1  hover:font-bold"
+            onClick={() => handleTabClick('product')}
+          >
+            <AiOutlineShopping className="mr-2" /> Products
+          </button>
+          <button
+            className="flex items-center  hover:underline underline-offset-1  hover:font-bold"
+            onClick={() => handleTabClick('categories')}
+          >
+            <AiOutlineTags className="mr-2" /> Categories
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
